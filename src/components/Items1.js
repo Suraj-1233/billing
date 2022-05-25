b04d6424d3f5e3c04e530e25614901328bd93fc8
suraj kannujiya <surajkannujiya517@gmail.com>
2022-05-25 22:53:40 +0530

    this is first time commint

---
+++ src/components/Items1.js
@@ -0,0 +1,44 @@
+
+
+const Items = ({ id, description, title, img, price, quantity }) => {
+ 
+
+  return (
+    <>
+      <div className="items-info">
+        
+        <div className="title">
+          <h2>PRODUCT  ITEMS </h2>
+          
+        </div>
+  
+          <div className="add-minus-quantity">
+          {/* <i className="fas fa-minus minus" onClick={() => decrement(id)}></i> */}
+          {/* <input type="text" placeholder="quantity" disabled /> */}
+          <div className="price">
+          <h3>Quantity</h3>
+        </div>
+          {/* <i className="fas fa-plus add" onClick={() => increment(id)}></i> */}
+          </div>
+
+
+
+
+        <div className="price">
+          <h3>₹ price</h3>
+        </div>
+        <div className="price">
+          <h3>₹ Total price</h3>
+        </div>
+        <div className="price">
+          <h3>Detete Items</h3>
+        </div>
+        
+      </div>
+
+      <hr />
+    </>
+  );
+};
+
+export default Items;
---
+++ src/components/products.js
@@ -0,0 +1,75 @@
+export const products = [
+  
+  {
+    id: 1,
+    title: "A",
+    description: "XYZ",
+    price: "2500",
+    
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "B",
+    description: "XYZ",
+    price: "2300",
+    
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "C",
+    description: "XYZ",
+    price: "3500",
+    
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "D",
+    description: "XYZ",
+    price: "90500",
+   
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "E",
+    description: "XYZ",
+    price: "2500",
+   
+    quantity: 1,
+  },
+  {
+    id: 6,
+    title: "F",
+    description: "XYZ",
+    price: "3500",
+  
+    quantity: 1,
+  },
+  {
+    id: 7,
+    title: "G",
+    description: "XYZ",
+    price: "2500",
+    
+    quantity: 1,
+  },
+  {
+    id: 8,
+    title: "H",
+    description: "XYZ",
+    price: "90500",
+
+    quantity: 1,
+  },
+  {
+    id: 9,
+    title: "I",
+    description: "XYZ",
+    price: "2500",
+   
+    quantity: 1,
+  },
+];
