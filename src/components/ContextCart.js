cd3c9b3961a37c504181388f172d6ee9bb82bf34
suraj kannujiya <surajkannujiya517@gmail.com>
2022-05-26 11:58:48 +0530

    last

--- src/components/ContextCart.js
+++ src/components/ContextCart.js
@@ -18,18 +18,18 @@ const ContextCart = () => {
             <h3>SURAJ KANNUJIYA</h3>
           </div>
 
-          <div className="cart-icon">
+          {/* <div className="cart-icon">
             <img src="./images/cart.png" alt="cart" />
             ++<p>{totalItem}</p>
-          </div>
+          </div> */}
         </header>
 
         <section className="main-cart-section">
           <h1>shopping Cart</h1>
-          <p className="total-items">
+          {/* <p className="total-items">
             you have <span className="total-items-count">{totalItem} </span>{" "}
             items in shopping cart
-          </p>
+          </p> */}
         </section>
       </>
     );
@@ -43,19 +43,19 @@ const ContextCart = () => {
           <h3> SURAJ KANNUJIYA </h3>
         </div>
 
-        <div className="cart-icon">
+        {/* <div className="cart-icon">
           <img src="./images/cart.png" alt="cart" />
           <p>{totalItem}</p>
-        </div>
+        </div> */}
       </header>
 
 
       <section className="main-cart-section">
         <h1>BILL</h1>
-        <p className="total-items">
+        {/* <p className="total-items">
           you have <span className="total-items-count">{totalItem} </span> items
           in shopping cart
-        </p>
+        </p> */}
 
          <Items1>
          </Items1>
